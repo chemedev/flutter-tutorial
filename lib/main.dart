01325d1427b69182234339d4052b1b20ed39e3b6
Juan Chemello <juanchemell@gmail.com>
2020-07-07 23:53:28 -0300

    containers-padding-rows-cols

--- lib/main.dart
+++ lib/main.dart
@@ -15,14 +15,23 @@ class Home extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.red[600],
       ),
-      body: Center(
-        child: IconButton(
-          onPressed: () {
-            print('you clicked me');
-          },
-          icon: Icon(Icons.alternate_email),
-          color: Colors.amber,
-        )
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: <Widget>[
+          Container(
+              padding: EdgeInsets.all(30.0),
+              color: Colors.pink,
+              child: Text('two')),
+          Container(
+              padding: EdgeInsets.all(40.0),
+              color: Colors.amber,
+              child: Text('three')),
+          Container(
+              padding: EdgeInsets.all(20.0),
+              color: Colors.cyan,
+              child: Text('one')),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
